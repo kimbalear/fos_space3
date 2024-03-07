94a90e1e7eeb5b0c47621e1554d489f9c40fa9b2
Venrique <00046517@uca.edu.sv>
2024-03-07 14:27:52 -0600

    changed course search from shortname to idnumber

--- layout/coursecategory.php
+++ layout/coursecategory.php
@@ -12,7 +12,7 @@ if (isloggedin() && !isguestuser()) {
     $modules = new stdClass();
 
     try{
-        $course = $DB->get_record('course', array('shortname' => 'CTN'));
+        $course = $DB->get_record('course', array('idnumber' => 'CTN'));
         
         $courseModulesObject = get_fast_modinfo($course->id);
         $courseModules = $courseModulesObject->get_cms();
